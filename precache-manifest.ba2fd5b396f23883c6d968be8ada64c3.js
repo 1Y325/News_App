4dd1e706ccecc4e98df1f4df8aae2015ce468bbd
Yash Gupta <yashg3600@gmail.com>
2022-07-13 20:44:28 +0530

    Updates

---
+++ precache-manifest.ba2fd5b396f23883c6d968be8ada64c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30b1789694b6be8caee4",
+    "url": "/News_App/static/css/main.e616d7b4.chunk.css"
+  },
+  {
+    "revision": "30b1789694b6be8caee4",
+    "url": "/News_App/static/js/main.30b17896.chunk.js"
+  },
+  {
+    "revision": "4bb05d8fed1e4d6e8559",
+    "url": "/News_App/static/js/1.4bb05d8f.chunk.js"
+  },
+  {
+    "revision": "894fe79290774edf9143",
+    "url": "/News_App/static/js/2.894fe792.chunk.js"
+  },
+  {
+    "revision": "07f0d02629937a3e2d6e",
+    "url": "/News_App/static/js/runtime~main.07f0d026.js"
+  },
+  {
+    "revision": "bd2189dd4d7aa607e15a143cc1c9def5",
+    "url": "/News_App/index.html"
+  }
+];
